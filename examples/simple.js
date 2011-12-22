4fd3f8784d366f06937dce91df73aa0f49e64f62
Damon Oehlman <damon.oehlman@sidelab.com>
2011-12-23 09:30:42 +1000

    Added changelog, hopefully I get into a good habit of filling it out

--- examples/simple.js
+++ examples/simple.js
@@ -7,6 +7,8 @@ machine = new cm.Machine('http://sidelab.iriscouch.com/seattle_neighbourhood');
 
 // perform actions for each of the 
 machine.on('enter', function(item) {
+    console.log('item ' + item.id + ' has entered the machine');
+    
     setTimeout(function() {
         item.done();
     }, Math.random() * 5000);
